38a2cbdd3f9fc2ae9f040b07e368ad794a5e6170
Eunsoojoe <joeun2264@gmail.com>
2024-07-23 11:13:39 +0900

    new

---
+++ 00_make_url.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv()
+
+TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+
+URL = f'https://api.telegram.org/bot{TOKEN}'
+
+method = 'getUpdates'
+
+res = requests.get(f'{URL}/{method}')
+res_dict = res.json()
+
+user_input = res_dict['result'][-1]['message']['text']
+user_id = res_dict['result'][-1]['message']['from']['id']
+
+print(user_id, user_input)
+print(f'{URL}/sendMessage?chat_id={user_id}&text={user_input}')
+
+SEND_MSG_URL = f'{URL}/sendMessage?chat_id={user_id}&text={user_input}'
+
+requests.get(SEND_MSG_URL)
